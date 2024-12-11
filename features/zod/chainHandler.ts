aec1638ea77ca429fab388a117bbf549afbbe192
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-12-11 18:13:33 +0000

    bug on hanlder error catch fixed

--- features/zod/chainHandler.ts
+++ features/zod/chainHandler.ts
@@ -6,6 +6,7 @@ import type {
 
 class ChainHandler {
   private middlewareError: Error | null;
+  private hanlderError: Error | null;
   private procedurePromise: Promise<any> | null = null;
   private hanlderPromise: Promise<any> | null = null;
   state: any;
@@ -18,15 +19,10 @@ class ChainHandler {
   procedure(callback: any): this {
     this.procedurePromise = callback();
     if (this.procedurePromise instanceof Promise) {
-      this.procedurePromise
-        .then((data) => {
-          this.state.ctx = data;
-          return data;
-        })
-        .catch((error) => {
-          this.middlewareError = error;
-          return this;
-        });
+      this.procedurePromise.then((data) => {
+        this.state.ctx = data;
+        return data;
+      });
     }
     return this;
   }
@@ -36,7 +32,6 @@ class ChainHandler {
       return this;
     }
     this.procedurePromise!.then((data) => {
-      console.log("this is from schema", this.state.input);
       this.state.schema = schema;
     });
     return this;
@@ -50,12 +45,23 @@ class ChainHandler {
     return this;
   }
 
-  handler(callback: any): this {
-    this.hanlderPromise = callback();
-    this.procedurePromise!.then((data) => {
-      this.hanlderPromise;
-    }).catch((err) => {
-      this.middlewareError = err;
+  handler(callback: ({ input, ctx }: { input?: any; ctx?: any }) => any): this {
+    this.hanlderPromise = new Promise((resolve, reject) => {
+      this.procedurePromise!.then(() => {
+        const result = callback({
+          ctx: this.state.ctx,
+          input: this.state.input,
+        })
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      }).catch((err) => {
+        this.hanlderError = err;
+        reject(err);
+      });
     });
 
     return this;
@@ -63,16 +69,14 @@ class ChainHandler {
 
   onSuccess(callback: ({ ctx, input }: { ctx: any; input: any }) => any) {
     Promise.all([this.procedurePromise, this.hanlderPromise]).then((data) => {
-      if (!this.middlewareError) {
-        console.log("from success inside", data);
-        callback({ ctx: this.state.ctx, input: this.state.input });
-      }
+      callback({ ctx: this.state.ctx, input: this.state.input });
     });
     return this;
   }
 
   onError(callback: ({ error }: { error: any }) => any) {
-    this.procedurePromise!.catch((err) => {
+    Promise.all([this.procedurePromise, this.hanlderPromise]).catch((err) => {
+      console.log(err);
       callback({ error: err });
     });
     return this;
@@ -84,87 +88,87 @@ export function chainHanlder() {
   return chain;
 }
 
-const chainHandler = <Result>(): ChainableHandler<unknown, Result, unknown> => {
-  const ctx: ChainableContext<unknown, Result, unknown> = {
-    isAuthed: false,
-    schema: null,
-    input: null,
-    middleware: null,
-    error: null,
-    result: null,
-  };
+// const chainHandler = <Result>(): ChainableHandler<unknown, Result, unknown> => {
+//   const ctx: ChainableContext<unknown, Result, unknown> = {
+//     isAuthed: false,
+//     schema: null,
+//     input: null,
+//     middleware: null,
+//     error: null,
+//     result: null,
+//   };
 
-  return {
-    async procedure(callback) {
-      try {
-        const middleware = await callback();
-        if (!middleware) throw new Error("middleware Error");
-        ctx.middleware = middleware;
-        ctx.isAuthed = true;
-      } catch (error) {
-        ctx.error = error as Error;
-      }
+//   return {
+//     async procedure(callback) {
+//       try {
+//         const middleware = await callback();
+//         if (!middleware) throw new Error("middleware Error");
+//         ctx.middleware = middleware;
+//         ctx.isAuthed = true;
+//       } catch (error) {
+//         ctx.error = error as Error;
+//       }
 
-      return this as ChainableHandler<
-        typeof ctx.input,
-        Result,
-        typeof ctx.middleware
-      >;
-    },
-    async schema(schema: ZodSchema) {
-      try {
-        if (procedurePromise) {
-          console.log("from schema");
-          await procedurePromise; // Wait for procedure to complete
-        }
-        if (ctx.error) return this;
-        ctx.schema = schema;
-      } catch (error) {
-        ctx.error = error as Error;
-      }
-      return this as ChainableHandler<
-        z.infer<typeof schema>,
-        Result,
-        typeof ctx.middleware
-      >;
-    },
-    async input(data) {
-      if (procedurePromise) await procedurePromise; // Wait for procedure to complete
-      if (ctx.error) return this;
-      if (!ctx.schema) throw new Error("Schema not defined");
-      const ss = ctx.schema.safeParse(data);
-      if (!ss.success) {
-        ctx.error = ss.error.errors as ZodIssue[];
-        return this;
-      }
-      ctx.input = ss;
-      return this;
-    },
-    async handler(callback) {
-      try {
-        if (procedurePromise) await procedurePromise; // Wait for procedure to complete
-        if (ctx.error) return this;
-        const result = callback(ctx.input!, ctx.middleware!);
-        ctx.result = result;
-      } catch (error) {
-        ctx.error = error as Error;
-      }
-      return this;
-    },
-    onError(callback) {
-      if (ctx.error) {
-        callback(ctx.error);
-      }
-      return this;
-    },
+//       return this as ChainableHandler<
+//         typeof ctx.input,
+//         Result,
+//         typeof ctx.middleware
+//       >;
+//     },
+//     async schema(schema: ZodSchema) {
+//       try {
+//         if (procedurePromise) {
+//           console.log("from schema");
+//           await procedurePromise; // Wait for procedure to complete
+//         }
+//         if (ctx.error) return this;
+//         ctx.schema = schema;
+//       } catch (error) {
+//         ctx.error = error as Error;
+//       }
+//       return this as ChainableHandler<
+//         z.infer<typeof schema>,
+//         Result,
+//         typeof ctx.middleware
+//       >;
+//     },
+//     async input(data) {
+//       if (procedurePromise) await procedurePromise; // Wait for procedure to complete
+//       if (ctx.error) return this;
+//       if (!ctx.schema) throw new Error("Schema not defined");
+//       const ss = ctx.schema.safeParse(data);
+//       if (!ss.success) {
+//         ctx.error = ss.error.errors as ZodIssue[];
+//         return this;
+//       }
+//       ctx.input = ss;
+//       return this;
+//     },
+//     async handler(callback) {
+//       try {
+//         if (procedurePromise) await procedurePromise; // Wait for procedure to complete
+//         if (ctx.error) return this;
+//         const result = callback(ctx.input!, ctx.middleware!);
+//         ctx.result = result;
+//       } catch (error) {
+//         ctx.error = error as Error;
+//       }
+//       return this;
+//     },
+//     onError(callback) {
+//       if (ctx.error) {
+//         callback(ctx.error);
+//       }
+//       return this;
+//     },
 
-    onSuccess(callback) {
-      if (!ctx.error) {
-        callback(ctx.result);
-      }
-      return this;
-    },
-  };
-};
+//     onSuccess(callback) {
+//       if (!ctx.error) {
+//         callback(ctx.result);
+//       }
+//       return this;
+//     },
+//   };
+// };
 
 // export default chainHandler();
--- index.ts
+++ index.ts
@@ -1,30 +1,49 @@
-import { getShebang } from "typescript";
 import { z } from "zod";
 import { chainHanlder } from "./features/zod/chainHandler";
+
+export async function getData(): Promise<{ id: string; title: string }> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const time = new Date();
+      const success = Math.random() > 0.5;
+      true
+        ? resolve({ id: "akd-iasd-2323-asds-", title: "this is a title" })
+        : reject(time);
+    }, 1000);
+  });
+}
 
 export async function getUser(): Promise<{ email: string; id: string }> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const success = Math.random() > 0.5; // Random success/failure
-      true
+      const time = new Date();
+      const success = Math.random() > 0.5;
+      false
         ? resolve({ id: "akd-123a", email: "email@example.com" })
-        : reject("Authentication failed");
+        : reject(time);
     }, 1000);
   });
 }
 
 const mySchema = z.object({ name: z.string(), lastname: z.string() });
 const myInput = { name: "tony", lastname: "hajdini" };
-const chain = chainHanlder()
+
+chainHanlder()
   .procedure(async () => {
     const user = getUser();
     return user;
   })
   .schema(mySchema)
   .input(myInput)
-  .handler(async () => {
-    const user = await getUser();
+  .handler(async ({ input, ctx }) => {
+    const tm = new Date();
+    console.log("from hanlder", ctx, tm);
+    const user = await getData();
     return user;
   })
-  .onError(({ error }) => {})
-  .onSuccess(({ ctx, input }) => {});
+  .onSuccess(({ ctx, input }) => {
+    console.log("from success", ctx);
+  })
+  .onError(({ error }) => {
+    console.log(error, "from error");
+  });
