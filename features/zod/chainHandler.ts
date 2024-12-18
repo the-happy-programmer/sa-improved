23a35724445f6a06ef259ced56d5b4c08ffc7684
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-12-18 00:26:37 +0000

    removed legacy code

--- features/zod/chainHandler.ts
+++ features/zod/chainHandler.ts
@@ -3,7 +3,7 @@ import type {
   ChainableHandler,
   ChainableContext,
 } from "../../types/zodChainHandler.types";
-// make them all async
+
 class ChainHandler {
   private middlewareError: Error | null;
   private handlerError: Error | null;
@@ -150,88 +150,3 @@ export function chainHanlder() {
   const chain = new ChainHandler();
   return chain;
 }
-
-// const chainHandler = <Result>(): ChainableHandler<unknown, Result, unknown> => {
-//   const ctx: ChainableContext<unknown, Result, unknown> = {
-//     isAuthed: false,
-//     schema: null,
-//     input: null,
-//     middleware: null,
-//     error: null,
-//     result: null,
-//   };
-
-//   return {
-//     async procedure(callback) {
-//       try {
-//         const middleware = await callback();
-//         if (!middleware) throw new Error("middleware Error");
-//         ctx.middleware = middleware;
-//         ctx.isAuthed = true;
-//       } catch (error) {
-//         ctx.error = error as Error;
-//       }
-
-//       return this as ChainableHandler<
-//         typeof ctx.input,
-//         Result,
-//         typeof ctx.middleware
-//       >;
-//     },
-//     async schema(schema: ZodSchema) {
-//       try {
-//         if (procedurePromise) {
-//           console.log("from schema");
-//           await procedurePromise; // Wait for procedure to complete
-//         }
-//         if (ctx.error) return this;
-//         ctx.schema = schema;
-//       } catch (error) {
-//         ctx.error = error as Error;
-//       }
-//       return this as ChainableHandler<
-//         z.infer<typeof schema>,
-//         Result,
-//         typeof ctx.middleware
-//       >;
-//     },
-//     async input(data) {
-//       if (procedurePromise) await procedurePromise; // Wait for procedure to complete
-//       if (ctx.error) return this;
-//       if (!ctx.schema) throw new Error("Schema not defined");
-//       const ss = ctx.schema.safeParse(data);
-//       if (!ss.success) {
-//         ctx.error = ss.error.errors as ZodIssue[];
-//         return this;
-//       }
-//       ctx.input = ss;
-//       return this;
-//     },
-//     async handler(callback) {
-//       try {
-//         if (procedurePromise) await procedurePromise; // Wait for procedure to complete
-//         if (ctx.error) return this;
-//         const result = callback(ctx.input!, ctx.middleware!);
-//         ctx.result = result;
-//       } catch (error) {
-//         ctx.error = error as Error;
-//       }
-//       return this;
-//     },
-//     onError(callback) {
-//       if (ctx.error) {
-//         callback(ctx.error);
-//       }
-//       return this;
-//     },
-
-//     onSuccess(callback) {
-//       if (!ctx.error) {
-//         callback(ctx.result);
-//       }
-//       return this;
-//     },
-//   };
-// };
-
-// export default chainHandler();
